6423387e27d168f283198508c750796e0d865912
ahmad <melato.1996@gmail.com>
2024-09-18 11:06:22 +0300

    Syrian Geeks App

--- app/src/main/java/com/melato/syriangeeks/ui/SignupActivity/FirstFragment.java
+++ app/src/main/java/com/melato/syriangeeks/ui/SignupActivity/FirstFragment.java
@@ -2,6 +2,7 @@ package com.melato.syriangeeks.ui.SignupActivity;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,9 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.melato.syriangeeks.R;
+import com.melato.syriangeeks.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
+    private FragmentFirstBinding binding;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -28,20 +31,19 @@ public class FirstFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-        Spinner spinner = (Spinner) view.findViewById(R.id.planets_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 getContext(),
                 R.array.gender_options,
                 R.layout.simple_spinner_item
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        return view;
+        binding.genderspinner.setAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        return binding.getRoot();
     }
 }
