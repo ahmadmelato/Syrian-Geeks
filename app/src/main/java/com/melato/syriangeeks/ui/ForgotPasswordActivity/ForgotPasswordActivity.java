06a08793ab7503c5dffd9c177e852dab051158f6
ahmad <melato.1996@gmail.com>
2024-09-23 14:31:18 +0300

    Syrian Geeks App

--- app/src/main/java/com/melato/syriangeeks/ui/ForgotPasswordActivity/ForgotPasswordActivity.java
+++ app/src/main/java/com/melato/syriangeeks/ui/ForgotPasswordActivity/ForgotPasswordActivity.java
@@ -1,26 +1,42 @@
 package com.melato.syriangeeks.ui.ForgotPasswordActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.melato.syriangeeks.R;
+import com.melato.syriangeeks.databinding.ActivityForgotPasswordBinding;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityForgotPasswordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_forgot_password);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.toolbar_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toolbar_back:
+                onBackPressed();
+                break;
+        }
     }
 }
