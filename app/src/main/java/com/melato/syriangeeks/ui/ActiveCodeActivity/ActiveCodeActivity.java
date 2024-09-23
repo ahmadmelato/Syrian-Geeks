8d937f6de380fe6d5d5895898a730b3643d12fa0
ahmad <melato.1996@gmail.com>
2024-09-23 14:40:02 +0300

    Syrian Geeks App

---
+++ app/src/main/java/com/melato/syriangeeks/ui/ActiveCodeActivity/ActiveCodeActivity.java
@@ -0,0 +1,26 @@
+package com.melato.syriangeeks.ui.ActiveCodeActivity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.melato.syriangeeks.R;
+
+public class ActiveCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_active_code);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
