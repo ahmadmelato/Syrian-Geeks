bb8bfa80ed554b875be2936eff65e7256e11f69a
ahmad <melato.1996@gmail.com>
2024-10-19 13:14:36 +0300

    Syrian Geeks App

--- app/src/main/java/com/melato/syriangeeks/ui/MainActivity/MainActivity.java
+++ app/src/main/java/com/melato/syriangeeks/ui/MainActivity/MainActivity.java
@@ -1,26 +1,63 @@
 package com.melato.syriangeeks.ui.MainActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
+import com.google.android.material.navigation.NavigationView;
 import com.melato.syriangeeks.R;
+import com.melato.syriangeeks.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding activityMainBinding;
+    private ImageButton toolbar_openmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        activityMainBinding.navView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, activityMainBinding.drawerLayout, null,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        activityMainBinding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        toolbar_openmenu = findViewById(R.id.toolbar_openmenu);
+        toolbar_openmenu.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toolbar_openmenu:
+                activityMainBinding.drawerLayout.open();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
