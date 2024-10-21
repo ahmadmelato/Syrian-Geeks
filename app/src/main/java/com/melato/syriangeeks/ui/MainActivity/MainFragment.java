ed909af3b4330f5c12b889b83c8d277fcd4d84a5
ahmad <melato.1996@gmail.com>
2024-10-21 12:25:22 +0300

    Syrian Geeks App

--- app/src/main/java/com/melato/syriangeeks/ui/MainActivity/MainFragment.java
+++ app/src/main/java/com/melato/syriangeeks/ui/MainActivity/MainFragment.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-import com.google.android.material.navigation.NavigationView;
 import com.melato.syriangeeks.R;
 import com.melato.syriangeeks.databinding.FragmentMainBinding;
 
@@ -22,15 +19,9 @@ import com.melato.syriangeeks.databinding.FragmentMainBinding;
 public class MainFragment extends Fragment implements View.OnClickListener {
 
     private FragmentMainBinding binding;
-    private ImageButton toolbar_openmenu;
 
     public MainFragment() {
         // Required empty public constructor
-    }
-
-    public static MainFragment newInstance() {
-        MainFragment fragment = new MainFragment();
-        return fragment;
     }
 
     @Nullable
@@ -44,23 +35,24 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
-        toolbar_openmenu = view.findViewById(R.id.toolbar_openmenu);
+        ImageButton toolbar_openmenu = view.findViewById(R.id.toolbar_openmenu);
+        ImageButton toolbar_notify = view.findViewById(R.id.toolbar_notify);
         toolbar_openmenu.setOnClickListener(this);
+        toolbar_notify.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.toolbar_openmenu:
-                MainActivity mainActivity = (MainActivity) getActivity();
-                mainActivity.openDrawerLayout();
-                break;
+        MainActivity mainActivity = (MainActivity) getActivity();
+        int id = v.getId();
+        if (id == R.id.toolbar_openmenu) {
+            assert mainActivity != null;
+            mainActivity.openDrawerLayout();
+        } else if (id == R.id.toolbar_notify) {
+            assert mainActivity != null;
+            mainActivity.openNotifications();
         }
     }
-
-
 
 }
